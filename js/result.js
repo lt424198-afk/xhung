0cfbc0adac4a1fd37274912599cfefbf637abb73
lt424198-afk <lt424198@gmail.com>
2026-03-17 03:54:29 +0700

    Update result.js

--- js/result.js
+++ js/result.js
@@ -1,23 +1,43 @@
+<section id="result"></section>
+
+<script>
+
+const books=[
+
+{title:"Dế Mèn Phiêu Lưu Ký",img:"https://upload.wikimedia.org/wikipedia/vi/3/3b/DeMen.jpg"},
+{title:"Hai Vạn Dặm Dưới Đáy Biển",img:"https://upload.wikimedia.org/wikipedia/commons/7/73/20000Leagues.jpg"},
+{title:"Không Gia Đình",img:"https://upload.wikimedia.org/wikipedia/commons/3/36/SansFamille.jpg"}
+
+]
+
 const params=new URLSearchParams(location.search)
 
 const name=params.get("name")
 
 const result=document.getElementById("result")
 
-if(name){
+books.forEach(b=>{
+
+if(b.title===name){
 
 result.innerHTML=`
 
 <div class="card">
 
-<h2>${name}</h2>
+<img src="${b.img}">
+
+<h2>${b.title}</h2>
 
 <p>Đây là trang thông tin truyện.</p>
 
-<a href="sach.html">← Quay lại thư viện</a>
+<a href="sach.html">← Quay lại</a>
 
 </div>
 
 `
 
 }
+
+})
+
+</script>
