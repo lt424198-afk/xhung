4305b73a587525b8367a008f0d8c7f487046034c
lt424198-afk <lt424198@gmail.com>
2026-03-17 03:54:02 +0700

    Update books.js

--- js/books.js
+++ js/books.js
@@ -6,13 +6,38 @@ img:"https://upload.wikimedia.org/wikipedia/vi/3/3b/DeMen.jpg"
 },
 
 {
-title:"Hai vạn dặm dưới đáy biển",
+title:"Hai Vạn Dặm Dưới Đáy Biển",
 img:"https://upload.wikimedia.org/wikipedia/commons/7/73/20000Leagues.jpg"
 },
 
 {
-title:"Không gia đình",
+title:"Không Gia Đình",
 img:"https://upload.wikimedia.org/wikipedia/commons/3/36/SansFamille.jpg"
+},
+
+{
+title:"Hoàng Tử Bé",
+img:"https://upload.wikimedia.org/wikipedia/en/0/05/Littleprince.JPG"
+},
+
+{
+title:"Alice Ở Xứ Sở Thần Tiên",
+img:"https://upload.wikimedia.org/wikipedia/commons/6/6f/Alice%27s_Adventures_in_Wonderland.jpg"
+},
+
+{
+title:"Totto-chan Bên Cửa Sổ",
+img:"https://upload.wikimedia.org/wikipedia/en/7/7c/Tottochan.jpg"
+},
+
+{
+title:"Những Cuộc Phiêu Lưu Của Tom Sawyer",
+img:"https://upload.wikimedia.org/wikipedia/commons/7/79/Tom_Sawyer_1876_frontispiece.jpg"
+},
+
+{
+title:"Đảo Giấu Vàng",
+img:"https://upload.wikimedia.org/wikipedia/commons/3/3b/TreasureIsland.jpg"
 }
 
 ]
@@ -33,7 +58,7 @@ container.innerHTML+=`
 
 <h3>${b.title}</h3>
 
-<a href="ketquatruyen.html?name=${b.title}">Xem</a>
+<a href="ketquatruyen.html?name=${b.title}">Xem truyện</a>
 
 </div>
 
@@ -44,7 +69,6 @@ container.innerHTML+=`
 }
 
 showBooks(books)
-
 
 document.getElementById("searchInput").addEventListener("input",function(){
 
